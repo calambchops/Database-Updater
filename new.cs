fd4b70f4096d101db0adac1d748d73c0c4e764aa
calambchops <36434516+calambchops@users.noreply.github.com>
2019-02-01 16:09:53 -0800

    Update
    restructured class, removed string[] members and replaced with one array and string members. Customized set accessor property to replace empty strings with a blank space to prevent empty flags. Consolidated for loops.

--- new.cs
+++ new.cs
@@ -8,15 +8,14 @@ using System.Text.RegularExpressions;
 
 //C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe /t:exe /out:\Users\ctrueman\Desktop\EXECUTE.exe C:\Users\ctrueman\Desktop\program\new.cs /r:C:\Users\ctrueman\Desktop\program\Microsoft.Office.Interop.Excel.dll /r:C:\Users\ctrueman\Desktop\program\Microsoft.Office.Interop.Access.dll
  
- 
 namespace RecordsUpdate
 {
 	public class Program
 	{
 		public static void Main()
 		{	
-			ExcelDataExtraction.OpenExcel();
-			DataBase.ConnectToDatabase();
+			CustomDataArrays customdataarrays = new CustomDataArrays();
+			customdataarrays.PopulateData();
 			
 			Console.WriteLine("Updates finished...");
 			Console.ReadLine();		
@@ -24,15 +23,12 @@ namespace RecordsUpdate
 	}
 	
 
-	public class ExcelDataExtraction
+	public static class ExcelDataExtraction
 	{
-	
-		public static int rowCount;
-				
-				
-		public static void OpenExcel()
+		
+		public static Excel.Workbook OpenExcel(Excel.Application excelApp)
 		{
-			Excel.Application excelApp = new Excel.Application();
+
 			string[] CompleteFile;
 
 			string path = @"W:\IOS\IA\CCU";
@@ -41,19 +37,15 @@ namespace RecordsUpdate
 			{			
 			
 				CompleteFile = GetDirectory(path);
-
 				Excel.Workbook excelWorkbook = excelApp.Workbooks.Open(CompleteFile[0], 0, true, 5, "", "", true, Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
-				Excel.Worksheet excelWorksheet = (Excel.Worksheet)excelWorkbook.Sheets[1];
-					
-				rowCount = FindLastRow(excelWorksheet);
-				CustomDataArrays.InitValues(rowCount);	
-				
-				SaveData(excelWorksheet, rowCount);
-				 	
-				excelWorkbook.Close();
-				excelApp.Quit();
-			}										
+
+				return excelWorkbook;
+			}
+			else
+				return null;
 		}
+		
+		
 		
 		
 		public static string[] GetDirectory(string path)
@@ -91,7 +83,6 @@ namespace RecordsUpdate
 				Console.WriteLine("Please enter a valid move sheet form!");
 				Console.WriteLine("Please enter a spreadsheet file to look for:");
 				
-				
 				FileName = Console.ReadLine();
 			}
 			
@@ -101,77 +92,59 @@ namespace RecordsUpdate
 		}
 	
 				
-		static void SaveData(Excel.Worksheet excelWorksheet, int rowCount)
-		{	
-			int row = 9;
-			string[] Name = new string[3];
+		
+		public static string SplitNameFirst(string[] Name)
+		{
 			
 			
-			for(int i=0; i < rowCount; i++)
-			{			 
-				 string TempName;
-				
-				 TempName = Convert.ToString(((Excel.Range)excelWorksheet.Cells[row, 4]).Value2);
-				 Name = TempName.Split(' ');
-
-				 if(Name.Length == 1)
+			 if(Name.Length == 1)
 				 {	 
-					 CustomDataArrays.last[i] = CustomDataArrays.first[i] = Name[0];			
+					 return Name[0];			
 				 }
 				 else
 				 {
-					 CustomDataArrays.first[i] = Name[0];
-					 CustomDataArrays.last[i] = Name[1];		 
+					 return Name[0]; 
+	 
 				 }	
-				 
-				 
-				 CustomDataArrays.division[i] = Convert.ToString(((Excel.Range)excelWorksheet.Cells[row, 3]).Value2);
-				 CustomDataArrays.number[i] = Convert.ToString(((Excel.Range)excelWorksheet.Cells[row, 6]).Value2);
-				 CustomDataArrays.riser[i] = Convert.ToString(((Excel.Range)excelWorksheet.Cells[row, 9]).Value2);
-				 CustomDataArrays.sio[i] = Convert.ToString(((Excel.Range)excelWorksheet.Cells[row, 11]).Value2);
-				 CustomDataArrays.cubicle[i] = Convert.ToString(((Excel.Range)excelWorksheet.Cells[row, 13]).Value2);
-				 
-				 //if cubicle is not known
-				 if(CustomDataArrays.cubicle[i].Length == 0)
-				 {
-					CustomDataArrays.cubicle[i] = " ";			
-				 }
-				 
-				 //if we forget to put division name
-				 if(CustomDataArrays.division[i].Length == 0)
-				 {
-					CustomDataArrays.division[i] = " ";			
-				 }
-				 
-				 
-				 string temp = CustomDataArrays.sio[i] = Convert.ToString(((Excel.Range)excelWorksheet.Cells[row, 11]).Value2);
-				 
-				 FloorNumber(i,temp);
-			
-				 DeterminePurpose(i,temp);
-						
-			     row++;			
-			}			
+		
 		}
 		
 		
-		static void DeterminePurpose(int i, string temp)
+		public static string SplitNameLast(string[] Name)
+		{
+			
+			 if(Name.Length == 1)
+				 {	 
+					 return Name[0];	
+			
+				 }
+				 else
+				 {
+					 return Name[1]; 
+	 
+				 }	
+		
+		}
+		
+		
+		public static string DeterminePurpose(int i, string temp)
 		{
 			string MyString = temp;
 			
 			 if(MyString == "MPOE")
 			 {
-				CustomDataArrays.purpose[i] = "LEFT AT MPOE";
-			 
+				return MyString;
+
 			 }
 			 
 			 else
-				CustomDataArrays.purpose[i] = "DESK PHONE";		
+				return "DESK PHONE";
+	
 		}
 
 
 		
-		static void FloorNumber(int i, string temp)
+		public static string FloorNumber(int i, string temp)
 		{
 		
 			 string MyString = temp; 
@@ -180,24 +153,25 @@ namespace RecordsUpdate
 			
 			 if(MyString == "MPOE")
 			 {				
-				CustomDataArrays.floor[i] = MyString;
-				return;
+				return MyString;
 			 }
 				
 			 else if(secondletter>='0' && secondletter<='9')
 			 {
-				CustomDataArrays.floor[i] = MyString.Substring(0,2);		 
+				return MyString.Substring(0,2);
+				 
 			 }
 			 
 			 else
 			 {
-				CustomDataArrays.floor[i] = MyString.Substring(0,1);
+				return MyString.Substring(0,1);
+				
 			 }
 		}
 		
 		
 		
-		static int FindLastRow(Excel.Worksheet excelWorksheet)
+		public static int FindLastRow(Excel.Worksheet excelWorksheet)
 		{	
 			 int Row = 9;   //starting point in excel sheet
 			 int Count = 0;
@@ -206,7 +180,7 @@ namespace RecordsUpdate
 			 while (ValidRow == true)
 			 {	   
 				Excel.Range Range = (Excel.Range)excelWorksheet.Cells[Row, 6]; 
-				//validates using phone number column, 6
+				//validates using phone number column
 		   
 				if (Range.Value != null)
 				{			
@@ -226,144 +200,231 @@ namespace RecordsUpdate
 	
 	public class DataBase
 	{	
-		public static void ConnectToDatabase()
+		public static OleDbConnection ConnectToDatabase()
 		{
-			string ConnStr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=W:\\IOS\\IA\\CCU\\Service Record DB\\Service.mdb;Jet OLEDB:Database Password=job;";
+			string ConnStr = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=W:\\IOS\\IA\\CCU\\Service Record DB\\Service.mdb;Jet OLEDB:Database Password=job;";
 			OleDbConnection MyConn = new OleDbConnection(ConnStr);
 			MyConn.Open();
 			
-			int changedValues = 0;
-			OleDbCommand Modify = null;
-
-			try
-			{			
-				string date = DateTime.Now.ToString("MM/dd/yyy");
-			
-				for(int j = 0; j < ExcelDataExtraction.rowCount; j++)
-				{
-					//put empty string into database					  
-					string cmd = "UPDATE [SERVICE MAIN TABLE] SET Divn='" + CustomDataArrays.division[j] + "',[First Name]='" + CustomDataArrays.first[j] + "',Purpose='" + CustomDataArrays.purpose[j] + "',[Last Name]='" + CustomDataArrays.last[j] + "', FLOOR='" + CustomDataArrays.floor[j] + "',RISER='" + CustomDataArrays.riser[j] + "',SIO='" + CustomDataArrays.sio[j] + "',CUBICLE='" + CustomDataArrays.cubicle[j] +  "',PCAUpdtDt='" + date + "',DivnChgDt='" + date + "' WHERE [Phone Nbr]= '" + CustomDataArrays.number[j] + "'";			
-					Modify = new OleDbCommand(cmd, MyConn);
-					changedValues = Modify.ExecuteNonQuery();	
-			
-				}
-
-			
-			}
-			
-			catch(Exception ex)
-			{
-				Console.WriteLine(ex.Message);	
-			}
-			
-			finally
-			{
-				MyConn.Close();		
-			}	
+			return MyConn;
 		}		
 	}
-		
-		
 	
-	
+		
 	public class CustomDataArrays
 	{
-		public static int row;
-		
-		 public static void InitValues(int rowCount)
-		 {		
-			row = rowCount;
-			p_division = new string[row];
-		    p_number = new string[row];
-		    p_floor = new string[row];
-		    p_riser = new string[row]; 
-			p_sio = new string[row];
-			p_cubicle = new string[row]; 
-			p_first = new string[row];
-			p_last = new string[row];
-			p_purpose = new string[row];
-		 }
+		 private string p_division;
+		 private string p_number;
+		 private string p_floor;
+		 private string p_riser; 
+		 private string p_sio;
+		 private string p_cubicle; 
+		 private string p_first;
+		 private string p_last;
+		 private string p_purpose;
 		 
-		 public static void print(){
-			
-			Console.WriteLine(CustomDataArrays.row);
-			
-		 }
-		 	 
-		 private static string[] p_division;
-		 private static string[] p_number;
-		 private static string[] p_floor;
-		 private static string[] p_riser; 
-		 private static string[] p_sio;
-		 private static string[] p_cubicle; 
-		 private static string[] p_first;
-		 private static string[] p_last;
-		 private static string[] p_purpose;
-			 
-		 public static string[] purpose{
 		 
+
+			
+		 public string Purpose
+		 {
 			get {return p_purpose;}
-			set {p_purpose = value;}
+			set {p_purpose = value;
+					
+					if(p_purpose == "")
+						p_purpose = " ";
+				
+				}
 		 
-			}
+		 }
 		 
-		 public static string[] division{
-		 
+		 public string Division
+		 {
+
 			get {return p_division;}
-			set {p_division = value;}
+			set {p_division = value;
+					
+					if(p_division == "")
+						p_division = " ";
+				
+				}
 		 
-			}
+		 }
 			
-		 public static string[] first{
-		 
+		 public string First
+		 {
+
 			get {return p_first;}
-			set {p_first = value;}
+			set {p_first = value;
+					
+					if(p_first == "")
+						p_first = " ";
+				
+				}
 		 
-			}
+		 
+		 }
 			
-		 public static string[] last{
-		 
+		 public string Last
+		 {
+
 			get {return p_last;}
-			set {p_last = value;}
+			set {p_last = value;
+					
+					if(p_last == "")
+						p_last = " ";
+				
+				}
 		 
-			}
+		 
+		 }
 		
-		 public static string[] number{
-		 
+		 public string Number
+		 {
+
 			get {return p_number;}
-			set {p_number = value;}
+			set {p_number = value;
+					
+					if(p_number == "")
+						p_number = " ";
+				
+				}
 		 
-			}
 		 
-		 public static string[] floor{
-
+		 }
+		 
+		 public string Floor
+		 {
+			
 			get {return p_floor;}
-			set {p_floor = value;}
-			
-			}
-			
-		 public static string[] riser{
+			set {p_floor = value;
+					
+					if(p_floor == "")
+						p_floor = " ";
+				
+				}
 		 
-			get {return p_riser;}
-			set {p_riser = value;}
 			
-			}
+		 }
 			
-		 public static string[] sio{
-		 
-			get {return p_sio;}
-			set {p_sio = value;}
-	
-			}
-			
-		 public static string[] cubicle {
-		 
-			get {return p_cubicle;}
-			set {p_cubicle = value;}
-		 
-			}
+		 public string Riser
+		 {
 
+			get {return p_riser;}
+			set {p_riser = value;
+					
+					if(p_riser == "")
+						p_riser = " ";
+				
+				}
+		 
+			
+		 }
+			
+		 public string Sio
+		 {
+
+			get {return p_sio;}
+			set {p_sio = value;
+					
+					if(p_sio == "")
+						p_sio = " ";
+				
+				}
+		 
+	
+		 }
+			
+		 public string Cubicle 
+		 {
+
+			get {return p_cubicle;}
+			set {p_cubicle = value;
+					
+					if(p_cubicle == "")
+						p_cubicle = " ";
+				
+				}
+		 
+		 
+		 }
+		
+		
+		 
+		 private CustomDataArrays[] point;	
+		 
+		 public void InitArray(int rowCount)
+		 {
+			point = new CustomDataArrays[rowCount]; //initialize array
+			
+			for(int i = 0; i < rowCount; i++)
+			{
+				point[i] = new CustomDataArrays(); //initialize each element
+			}	
+		 }
+		 
+ 
+
+		public void PopulateData()
+		{	
+			int row = 9;
+			int i;
+			string[] Name = new string[3];
+			string date = DateTime.Now.ToString("MM/dd/yyy");
+			OleDbConnection MyConn = DataBase.ConnectToDatabase();
+			int changedValues = 0;
+			OleDbCommand Modify = null;
+			Excel.Application excelApp = new Excel.Application();
+			Excel.Workbook excelWorkbook = ExcelDataExtraction.OpenExcel(excelApp);
+			Excel.Worksheet excelWorksheet = (Excel.Worksheet)excelWorkbook.Sheets[1];
+			int rowCount = ExcelDataExtraction.FindLastRow(excelWorksheet);
+			
+			InitArray(rowCount);
+			
+			for(i = 0; i < rowCount; i++)
+			{			 
+			
+				 string TempName = Convert.ToString(((Excel.Range)excelWorksheet.Cells[row, 4]).Value2);
+				 Name = TempName.Split(' ');
+				 
+				 string sio = Convert.ToString(((Excel.Range)excelWorksheet.Cells[row, 11]).Value2);
+				 string temp = sio;				 
+				 point[i].Sio = sio;
+ 				 point[i].First = ExcelDataExtraction.SplitNameFirst(Name);
+				 point[i].Last = ExcelDataExtraction.SplitNameLast(Name);
+				 point[i].Floor = ExcelDataExtraction.FloorNumber(i,temp);
+				 point[i].Purpose = ExcelDataExtraction.DeterminePurpose(i,temp);
+				 point[i].Division = Convert.ToString(((Excel.Range)excelWorksheet.Cells[row, 3]).Value2);
+				 point[i].Number = Convert.ToString(((Excel.Range)excelWorksheet.Cells[row, 6]).Value2);
+				 point[i].Riser = Convert.ToString(((Excel.Range)excelWorksheet.Cells[row, 9]).Value2);
+				 point[i].Cubicle = Convert.ToString(((Excel.Range)excelWorksheet.Cells[row, 13]).Value2);
+
+				 try
+				 {
+					 string cmd = "UPDATE [SERVICE MAIN TABLE] SET Divn='" + point[i].Division + "',[First Name]='" + point[i].First + "',Purpose='" + point[i].Purpose + "',[Last Name]='" + point[i].Last + "', FLOOR='" + point[i].Floor + "',RISER='" + point[i].Riser + "',SIO='" + point[i].Sio + "',CUBICLE='" + point[i].Cubicle +  "',PCAUpdtDt='" + date + "',DivnChgDt='" + date + "' WHERE [Phone Nbr]= '" + point[i].Number + "'";			
+					 Modify = new OleDbCommand(cmd, MyConn);
+					 changedValues = Modify.ExecuteNonQuery();	
+				 }
+	
+				 catch(Exception ex)
+			     {
+				  Console.WriteLine(ex.Message);	
+				  
+				 }
+				 
+			     row++;			
+			}	
+			
+			MyConn.Close();
+			excelWorkbook.Close();
+			excelApp.Quit();
+		}
+ 
+ 
 	}
+
+
+	
 	
 }
 	
